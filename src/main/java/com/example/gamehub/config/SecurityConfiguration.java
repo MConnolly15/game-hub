f914612335b940e6dd0cf2fe9782a975286580dc
Michael <mconnolly.se@gmail.com>
2026-09-14 21:07:43 +0100

    Add Auth0 logout functionality

--- src/main/java/com/example/gamehub/config/SecurityConfiguration.java
+++ src/main/java/com/example/gamehub/config/SecurityConfiguration.java
@@ -1,24 +1,47 @@
 package com.example.gamehub.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Value("${okta.oauth2.issuer}")
+  private String issuer;
 
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(withDefaults());
+  @Value("${okta.oauth2.client-id}")
+  private String clientId;
 
-        return http.build();
-    }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .oauth2Login(withDefaults())
+        .logout(logout -> logout.addLogoutHandler(logoutHandler()));
+
+    return http.build();
+  }
+
+  private LogoutHandler logoutHandler() {
+
+    return (request, response, authentication) -> {
+      try {
+
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+
+        response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    };
+  }
 }
