f3bb33c6f6864a481b4807454a2f4f8a5992801d
Kyle Peden <kpeden.dev@proton.me>
2026-09-14 16:10:12 +0100

    Adjusting Home for spotless check to work

--- src/main/java/com/example/gamehub/controller/HomePageController.java
+++ src/main/java/com/example/gamehub/controller/HomePageController.java
@@ -2,9 +2,13 @@ package com.example.gamehub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomePageController {
-    @RequestMapping("/")
-    public Redirect
+
+  @RequestMapping("/")
+  public RedirectView redirect() {
+    return new RedirectView("/home");
+  }
 }
