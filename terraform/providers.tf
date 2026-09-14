7289191752c8e3692d2eb83967f53955bd430e59
Kyle Peden <kpeden.dev@proton.me>
2026-09-14 16:03:06 +0100

    Fixing linting

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 
-    default_tags {
-      tags = {
-        Project = var.project_name
-        Environment = var.environment
-      }
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
     }
-  
+  }
+
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
-    type = string
-    default = "eu-west-2"  
+  type    = string
+  default = "eu-west-2"
 }
 
 variable "environment" {
-    type = string
-    default = "dev"
+  type    = string
+  default = "dev"
 }
 
 variable "project_name" {
-  type = string
+  type    = string
   default = "Game Hive"
 }
