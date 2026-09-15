1c596d97b2be4c6a83842b2061201944669d3b3f
Michael <58251832+MConnolly15@users.noreply.github.com>
2026-09-15 14:20:50 +0100

    Feature/username (#11)
    * Add usernames to user accounts
    
    * fix formatting checks to pass deploy checks
    
    * fix formatting after merge

--- src/main/java/com/example/gamehub/controller/HomePageController.java
+++ src/main/java/com/example/gamehub/controller/HomePageController.java
@@ -9,32 +9,27 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomePageController {
 
-  //giving controller access to users table
+  // giving controller access to users table
   private final UserRepository userRepository;
 
-  //automatically passes the UserRepository into this controller
+  // automatically passes the UserRepository into this controller
   public HomePageController(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
-
   @GetMapping("/")
-  public ModelAndView homePage( Authentication authentication) {
+  public ModelAndView homePage(Authentication authentication) {
     ModelAndView modelAndView = new ModelAndView("homepage");
 
     // get the email of the person who is logged in
     String email = authentication.getName();
 
-    //Find that user in the database
+    // Find that user in the database
     userRepository
-            .findByEmail(email)
+        .findByEmail(email)
 
-            // If we find them, send their username to the homepage
-            .ifPresent(
-                    user ->
-                            modelAndView.addObject(
-                                    "username",
-                                    user.getUsername()));
+        // If we find them, send their username to the homepage
+        .ifPresent(user -> modelAndView.addObject("username", user.getUsername()));
     // Show the homepage
     return modelAndView;
   }
--- src/main/java/com/example/gamehub/repository/UserRepository.java
+++ src/main/java/com/example/gamehub/repository/UserRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
+
   boolean existsByUsernameIgnoreCase(String username);
 }
