91899f5a7b76edf7ed9364df3c09869e2131c22d
Michael <mconnolly.se@gmail.com>
2026-09-15 13:58:46 +0100

    fix formatting checks to pass deploy checks

--- src/main/java/com/example/gamehub/controller/AccountController.java
+++ src/main/java/com/example/gamehub/controller/AccountController.java
@@ -1,10 +1,10 @@
 package com.example.gamehub.controller;
 
-import org.springframework.ui.Model;
 import com.example.gamehub.model.User;
 import com.example.gamehub.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,12 +27,15 @@ public class AccountController {
 
   @PostMapping("/register")
   public String registerUser(
-      @RequestParam String username, @RequestParam String email, @RequestParam String password,
+      @RequestParam String username,
+      @RequestParam String email,
+      @RequestParam String password,
       Model model) {
 
-    if(userRepository.existsByUsernameIgnoreCase(username)) {
+    if (userRepository.existsByUsernameIgnoreCase(username)) {
       model.addAttribute("usernameError", "Username already taken.");
-    return "register";}
+      return "register";
+    }
     String hashedPassword = passwordEncoder.encode(password);
 
     User user = new User(username, email, hashedPassword);
