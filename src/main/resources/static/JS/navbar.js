4fa7fc7d5c3318344cf2c84ef12b9fa14f04f50c
Kyle Peden <kpeden.dev@proton.me>
2026-09-15 16:02:36 +0100

    Revert "Merge pull request #14 from MConnolly15/Navbar-setup"
    This reverts commit 0fc08dd266fa79b01a24402a8c82a3091cdbdc99, reversing
    changes made to 48b3416321df1a75e5b7f13f0b1936ebbfc2888c.

--- src/main/resources/static/JS/navbar.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.getElementById("navToggle");
-const navMenu = document.getElementById("navMenu");
-
-navToggle.addEventListener('click', function (e){
-    navMenu.classList.toggle('active');
-});
