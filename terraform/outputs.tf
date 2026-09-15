1af0bd472d30fbb40acf19cadb2b1f12a89ab2a7
Kyle Peden <kpeden.dev@proton.me>
2026-09-15 15:32:45 +0100

    Connecting to AWS via terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "aws_account_id" {
+    description = "AWS ID"
+    value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_caller_arn"{
+    description = "ARN of identity"
+    value = data.aws_caller_identity.current.arn
+}
