f3431f160b0cb289bf0cfd3cd69bf821524f824c
Michael <mconnolly.se@gmail.com>
2026-09-15 14:19:22 +0100

    fix formatting after merge

--- src/main/java/com/example/gamehub/controller/AccountController.java
+++ src/main/java/com/example/gamehub/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.example.gamehub.controller;
 
-import org.springframework.ui.Model;
 import com.example.gamehub.model.User;
 import com.example.gamehub.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
