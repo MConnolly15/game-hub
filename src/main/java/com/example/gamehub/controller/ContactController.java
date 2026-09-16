e4af3a8dcc1c1fbd1a86034bf51a91c8e973d659
Katie Brown <katiejjbrown@outlook.com>
2026-09-16 11:08:29 +0100

    Added footer static pages, controllers and a static page CSS file

---
+++ src/main/java/com/example/gamehub/controller/ContactController.java
@@ -0,0 +1,29 @@
+package com.example.gamehub.controller;
+
+import com.example.gamehub.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ContactController {
+    private final UserRepository userRepository;
+
+    public ContactController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/contactus")
+    public ModelAndView contactPage(Authentication authentication) {
+
+        ModelAndView modelAndView = new ModelAndView("contactpage");
+
+        String email = authentication.getName();
+        userRepository
+                .findByEmail(email)
+                .ifPresent(user -> modelAndView.addObject("username", user.getUsername()));
+
+        return modelAndView;
+    }
+}
