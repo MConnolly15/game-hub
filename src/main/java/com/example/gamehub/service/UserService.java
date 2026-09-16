002b90b4df6c3ac92d3e10ef0375f9ddcf182382
DaphnaAzari <181016677+DaphnaAzari@users.noreply.github.com>
2026-09-16 16:06:34 +0100

    Added a UserService with updateUser method

--- src/main/java/com/example/gamehub/service/UserService.java
+++ src/main/java/com/example/gamehub/service/UserService.java
@@ -1,4 +1,23 @@
 package com.example.gamehub.service;
 
+import com.example.gamehub.model.User;
+import com.example.gamehub.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void updateUsername(String email, String newUsername){
+        User user = userRepository.findByEmail(email).orElseThrow();
+        user.setUsername(newUsername);
+    }
 }
+
--- src/test/java/com/example/gamehub/service/UserServiceTest.java
+++ src/test/java/com/example/gamehub/service/UserServiceTest.java
@@ -1,4 +1,34 @@
 package com.example.gamehub.service;
 
-public class UserServiceTest {
+import com.example.gamehub.model.User;
+import com.example.gamehub.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+//Turns on Mockito's machinery for this test class and without it,
+// @Mock wouldn't actually create anything
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    void successfullyUpdatesUsername() {
+        //creating a user object
+        User existingUser = new User("oldUsername", "daphna@example.com", "hashedPassword");
+        //when using the email to search give me back the full above details for user,
+        // this simulates the db finding a user
+        when(userRepository.findByEmail("daphna@example.com"))
+                .thenReturn(java.util.Optional.of(existingUser));
+        //creates a class under testing handling the fake repo via the constructor (like AccountController)
+        UserService userService = new UserService(userRepository);
+        userService.updateUsername("daphna@example.com", "newUsername");
+        //updated the username:
+        assertEquals("newUsername", existingUser.getUsername());
+    }
 }
