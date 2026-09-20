609e476a9f9bcaece2e2c27b468f909f8e9ae8c1
Michael <mconnolly.se@gmail.com>
2026-09-20 19:51:22 +0100

    Build rocket rush core game logic with tests

--- src/main/resources/static/rocket-rush/js/rocket-rush-logic.js
+++ src/main/resources/static/rocket-rush/js/rocket-rush-logic.js
@@ -1,10 +1,91 @@
-export function startGame(game){
+export function startGame(game) {
     game.running = true;
     game.countdown = 3;
     game.status = "countdown";
+    game.lives = 3;
+
 }
 
-export function gameCountdown(game){
+export function gameCountdown(game) {
     game.countdown--;
-    game.status = "playing";
+
+    if(game.countdown === 0){
+       game.status = "playing";
+   } else {
+       game.status = "countdown";
+   }
+
+}
+
+export function gameTimer(game) {
+    if (game.status === "playing") {
+        game.timer++;
+    }
+}
+
+export function createRocket(game) {
+    game.rocket = {};
+}
+
+export function moveRocketRight(game) {
+    if (game.rocket.x + game.rocket.speed <= 800) {
+        game.rocket.x += game.rocket.speed;
+    }
+}
+
+export function moveRocketLeft(game) {
+    if (game.rocket.x - game.rocket.speed >= 0) {
+        game.rocket.x -= game.rocket.speed;
+    }
+}
+
+export function loseLife(game){
+    game.lives--;
+
+    if(game.lives === 0){
+        game.status = "gameOver";
+    }
+}
+
+export function createBullet(game){
+    game.bullets.push({});
+}
+
+export function moveBullet(game) {
+    game.bullets[0].y -= game.bullets[0].speed;
+}
+
+export function bulletHitAsteroid(game) {
+    const bullet = game.bullets[0];
+    const asteroid = game.asteroids[0];
+
+    if (bullet.x === asteroid.x && bullet.y === asteroid.y) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export function createAsteroid(game){
+    game.asteroids.push({});
+}
+
+export function destroyAsteroid(game) {
+    //0 = start at first item of the array
+    //1 = remove one item from array
+    game.asteroids.splice(0, 1);
+}
+
+export function addScore(game){
+    game.score += 10;
+}
+
+export function removeScore(game){
+    game.score -= 10;
+}
+
+export function restartGame(game) {
+    startGame(game);
+    game.timer = 0;
+    game.score = 0;
 }
--- src/test/javascript/rocket-rush/rocket-rush-logic.test.js
+++ src/test/javascript/rocket-rush/rocket-rush-logic.test.js
@@ -1,4 +1,20 @@
-import { startGame, gameCountdown } from "../../../main/resources/static/rocket-rush/js/rocket-rush-logic.js";
+import {
+    startGame,
+    gameCountdown,
+    gameTimer,
+    createRocket,
+    moveRocketRight,
+    moveRocketLeft,
+    loseLife,
+    createBullet,
+    moveBullet,
+    bulletHitAsteroid,
+    createAsteroid,
+    destroyAsteroid,
+    addScore,
+    removeScore,
+    restartGame
+    } from "../../../main/resources/static/rocket-rush/js/rocket-rush-logic.js";
 import { describe, expect, it } from "vitest";
 
 describe("Rocket Rush game logic", () => {
@@ -70,34 +86,245 @@ describe("Rocket Rush game logic", () => {
         expect(game.status).toBe("playing");
     });
 
-    // Test 6 - Game timer starts
+    // Test 6 - Game timer increases while playing
+    it("game timer goes up when the game starts playing", () => {
+        const game = {
+            status: "playing",
+            timer: 0
+        }
+
+        gameTimer(game);
+
+        expect(game.timer).toBe(1);
+    });
+
+    // Test 6B - Timer does not increase when game is not playing
+    it("Game timer does not increase when player is not playing the game", () => {
+        const game = {
+            status: "menu",
+            timer: 0
+        }
+
+        gameTimer(game);
+
+        expect(game.timer).toBe(0);
+    });
 
     // Test 7 - Rocket is created
+    it("Creating a game rocket", () =>{
+        const game = {
+            rocket: null
+        };
+
+        createRocket(game);
+
+        expect(game.rocket).not.toBeNull();
+    });
 
     // Test 8 - Rocket moves right
+    it("Rocket moves right" , () => {
+        const game = {
+            rocket: {
+                x: 100,
+                speed: 5
+            }
+        }
+
+        moveRocketRight(game);
+
+        expect(game.rocket.x).toBe(105);
+    });
+
 
     // Test 9 - Rocket moves left
+    it("Rocket moves left" , () => {
+        const game = {
+            rocket: {
+                x: 100,
+                speed: 5
+            }
+        }
 
-    // Test 10 - Rocket stays inside game area
+        moveRocketLeft(game);
+
+        expect(game.rocket.x).toBe(95);
+
+    });
+
+    // Test 10A - Rocket stays inside game area RIGHT
+    it("Rocket stays inside the game area when it hits RIGHT edge" , () => {
+        const game = {
+            rocket: {
+                x: 800,
+                speed: 5
+            }
+        }
+
+        moveRocketRight(game);
+        expect(game.rocket.x).toBe(800);
+    });
+
+    // Test 10A - Rocket stays inside game area LEFT
+    it("Rocket stays inside canvas when it hits LEFT edge" , () => {
+        const game = {
+            rocket: {
+                x: 0,
+                speed: 5
+            }
+        }
+
+        moveRocketLeft(game);
+
+        expect(game.rocket.x).toBe(0);
+    });
 
     // Test 11 - Player starts with 3 lives
+    it("Player starts with 3 lives when game starts", () => {
+        const game = {
+            lives: 0
+        }
+
+        startGame(game);
+
+        expect(game.lives).toBe(3);
+
+    });
 
     // Test 12 - Player loses a life
+    it("deducts one life from the player", () => {
+        const game = {
+            lives: 3
+        }
+
+        loseLife(game);
+        expect(game.lives).toBe(2);
+    } );
 
     // Test 13 - Game over at 0 lives
+    it("game over displays when the player has no lives left",  () => {
+        const game = {
+            lives: 1,
+            status: "playing"
+        }
+
+        loseLife(game);
+        expect(game.status).toBe("gameOver");
+    });
 
     // Test 14 - Bullet is created
+    it("players bullet is created", () =>{
+        const game = {
+            bullets: []
+        }
+
+        createBullet(game);
+        expect(game.bullets.length).toBe(1);
+    });
 
     // Test 15 - Bullet moves
+    it("bullet shoots upwards", () => {
+        const game = {
+            bullets: [
+                {
+                    y: 500,
+                    speed: 10
+                }
+            ]
+        }
 
-    // Test 16 - Bullet hits asteroid
+        moveBullet(game);
+        expect(game.bullets[0].y).toBe(490);
+    });
 
-    // Test 17 - Asteroid is destroyed
+    // Test 16 - Asteroid is created
+    it("Asteroid is created", () => {
+        const game = {
+            asteroids: []
+        }
 
-    // Test 18 - Score increases
+        createAsteroid(game);
+        expect(game.asteroids.length).toBe(1);
+    });
 
-    // Test 19 - Score decreases on collision
+    // Test 17 - Bullet hits asteroid
+    it("bullet hits the asteroid", () => {
+        const game = {
+            bullets: [
+                {
+                    x: 100,
+                    y: 100,
+                }
+            ],
+            asteroids: [
+                {
+                    x: 100,
+                    y: 100
+                }
+            ]
+        }
 
-    // Test 20 - Restart resets game
+        const result = bulletHitAsteroid(game);
+        expect(result).toBe(true);
 
+    });
+
+    // Test 18 - Asteroid is destroyed
+    it("asteroid is destroyed after a hit", () => {
+        const game = {
+            asteroids: [
+                {}
+            ]
+        }
+
+        destroyAsteroid(game);
+        expect(game.asteroids.length).toBe(0);
+    });
+
+    // Test 19 - Score increases
+    it("score increases when player destroys an asteroid", () => {
+        const game = {
+            score: 0
+        }
+
+        addScore(game);
+        expect(game.score).toBe(10);
+    });
+
+    // Test 20 - Score decreases on collision
+    it("score decreases when a player is hit", () =>{
+        const game = {
+            score: 20
+        }
+
+        removeScore(game);
+        expect(game.score).toBe(10);
+    });
+
+    // Test 21 - Restart resets game
+    it("restarting the game resets the important values", () => {
+        const game = {
+            status: "gameOver",
+            score: 40,
+            lives: 0,
+            timer: 25
+        }
+
+        restartGame(game);
+        expect(game.status).toBe("countdown");
+        expect(game.score).toBe(0);
+        expect(game.lives).toBe(3);
+        expect(game.timer).toBe(0);
+    })
 });
+
+// Test 22 - game stays in countdown while countdown is above 0
+it("game stays in countdown while countdown is above 0", () => {
+    const game = {
+        status: "countdown",
+        countdown: 3
+    }
+
+    gameCountdown(game);
+    expect(game.countdown).toBe(2);
+    expect(game.status).toBe("countdown");
+})
